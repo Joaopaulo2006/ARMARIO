b689f6b54ad8c801ce29e21006e0a3289510bc18
João p s Lins <125692510+Joaopaulo2006@users.noreply.github.com>
2023-05-10 08:42:43 -1200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-function atualizar() {
-    const arma = [
-        Estado1 = { 
-            Estado: "Ocupado"
-        },
-        Estado2 = {
-            Estado: "Desocupado"
-        },
-        Estado3 = {
-            Estado: "Manutenção"
-        }
-    ];
-    const ocupado = document.querySelector("estado1");
-    var mudaplvrnTela = "";
-    ocupado = parseInt(Math.random()*arma.length);
+function Ocupado() {
+    var txt = document.getElementById("texto");
+    let ocupado = document.getElementById("pop-up");
+    txt.innerHTML = " Armario Ocupado";
+    ocupado.style.display = 'block';
+
+}
+function Manutencao() {
+    alert("No momento o armário está em manutenção")
+    var txt = document.getElementById("texto");
+    let manutencao = document.getElementById("pop-up");
+    txt.innerHTML = " Armario em Manutenção";
+    manutencao.style.display = 'block';
+    txt.className = 'manutencao';
     
-    document.getElementById("Estado1").innerHTML = ocupado;
-
-
+}
+function Desocupado() {
+    let desocupado = document.getElementById("pop-up");
+   
+}
+function fechar() {
+    document.getElementById("pop-up").style.display = 'none'
 }
