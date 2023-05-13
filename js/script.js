b0e2d2b3d8ab5222a2d3636e1cd6abc9f7471151
Joaopaulo2006 <jpo54545@gmail.com>
2023-05-13 10:45:52 -0300

    Finalização do projeto

--- js/script.js
+++ js/script.js
@@ -1,30 +1,210 @@
-function Desocupado() {
-    var estadoDes = document.querySelector("estado2");
-    
-    if(estadoDes.innerHTML === "Desocupado") {
-        destOcu.style.backgroudColor = "rgb(145, 143, 143)";    
-        estadoDes.innerHTML === "Ocupado";
-        estadoDes.className = "destOcu";
+function Ocupado1() {
+    var TxtEstado1 = document.querySelector('.ocu1');
+    if (TxtEstado1.innerHTML === "Ocupado") {
+        TxtEstado1.innerHTML = "Manutenção";
+        TxtEstado1.style.backgroundColor = 'rgb(196, 0, 0)';
     }
-   
+    else if (TxtEstado1.innerHTML === "Manutenção") {
+        TxtEstado1.innerHTML = "Desocupado";
+        TxtEstado1.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+    else {
+        TxtEstado1.innerHTML = "Ocupado";
+        TxtEstado1.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
 }
-function Ocupado() {
-    if(estadoOcu === "Ocupado") {
-        alert("No momento o armário esta ocupado");
+function Ocupado2() {
+    var TxtEstado1 = document.querySelector('.ocu2');
+    if (TxtEstado1.innerHTML === "Ocupado") {
+        TxtEstado1.innerHTML = "Manutenção";
+        TxtEstado1.style.backgroundColor = 'rgb(196, 0, 0)';
     }
-    var estadoOcu = document.querySelector("estado1");
-    if(estadoOcu === "Ocupado") {
-        estadoOcu.textContent = "Manutenção";
-        estadoOcu.style.backgroudColor = "rgb(196, 0, 0)";
+    else if (TxtEstado1.innerHTML === "Manutenção") {
+        TxtEstado1.innerHTML = "Desocupado";
+        TxtEstado1.style.backgroundColor = 'rgb(60, 161, 2)';
     }
-
-
+    else {
+        TxtEstado1.innerHTML = "Ocupado";
+        TxtEstado1.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
 }
-function Manutencao() {
-    if(estadoMan === "Manutenção") {
-    alert("No momento o armário está em manutenção");
+function Ocupado3() {
+    var TxtEstado1 = document.querySelector('.ocu3');
+    if (TxtEstado1.innerHTML === "Ocupado") {
+        TxtEstado1.innerHTML = "Manutenção";
+        TxtEstado1.style.backgroundColor = 'rgb(196, 0, 0)';
     }
-   
-    var estadoMan = document.getElementById("estado3");
-    
+    else if (TxtEstado1.innerHTML === "Manutenção") {
+        TxtEstado1.innerHTML = "Desocupado";
+        TxtEstado1.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+    else {
+        TxtEstado1.innerHTML = "Ocupado";
+        TxtEstado1.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+}
+function Ocupado4() {
+    var TxtEstado1 = document.querySelector('.ocu4');
+    if (TxtEstado1.innerHTML === "Ocupado") {
+        TxtEstado1.innerHTML = "Manutenção";
+        TxtEstado1.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+    else if (TxtEstado1.innerHTML === "Manutenção") {
+        TxtEstado1.innerHTML = "Desocupado";
+        TxtEstado1.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+    else {
+        TxtEstado1.innerHTML = "Ocupado";
+        TxtEstado1.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+}
+function Desocupado1() {
+    var TxtEstado2 = document.querySelector('.des1');
+    if (TxtEstado2.innerHTML === "Desocupado") {
+        TxtEstado2.innerHTML = "Ocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else if (TxtEstado2.innerHTML === "Ocupado") {
+        TxtEstado2.innerHTML = "Manutenção";
+        TxtEstado2.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+    else {
+        TxtEstado2.innerHTML = "Desocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+}
+function Desocupado2() {
+    var TxtEstado2 = document.querySelector('.des2');
+    if (TxtEstado2.innerHTML === "Desocupado") {
+        TxtEstado2.innerHTML = "Ocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else if (TxtEstado2.innerHTML === "Ocupado") {
+        TxtEstado2.innerHTML = "Manutenção";
+        TxtEstado2.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+    else {
+        TxtEstado2.innerHTML = "Desocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+}
+function Desocupado3() {
+    var TxtEstado2 = document.querySelector('.des3');
+    if (TxtEstado2.innerHTML === "Desocupado") {
+        TxtEstado2.innerHTML = "Ocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else if (TxtEstado2.innerHTML === "Ocupado") {
+        TxtEstado2.innerHTML = "Manutenção";
+        TxtEstado2.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+    else {
+        TxtEstado2.innerHTML = "Desocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+}
+function Desocupado4() {
+    var TxtEstado2 = document.querySelector('.des4');
+    if (TxtEstado2.innerHTML === "Desocupado") {
+        TxtEstado2.innerHTML = "Ocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else if (TxtEstado2.innerHTML === "Ocupado") {
+        TxtEstado2.innerHTML = "Manutenção";
+        TxtEstado2.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+    else {
+        TxtEstado2.innerHTML = "Desocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+}
+function Desocupado5() {
+    var TxtEstado2 = document.querySelector('.des5');
+    if (TxtEstado2.innerHTML === "Desocupado") {
+        TxtEstado2.innerHTML = "Ocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else if (TxtEstado2.innerHTML === "Ocupado") {
+        TxtEstado2.innerHTML = "Manutenção";
+        TxtEstado2.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+    else {
+        TxtEstado2.innerHTML = "Desocupado";
+        TxtEstado2.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+}
+function Manutencao1() {
+    var TxtEstado3 = document.querySelector('.manu1');
+    if (TxtEstado3.innerHTML === "Manutenção") {
+        TxtEstado3.innerHTML = "Desocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+    else if (TxtEstado3.innerHTML === "Desocupado") {
+        TxtEstado3.innerHTML = "Ocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else {
+        TxtEstado3.innerHTML = "Manutenção";
+        TxtEstado3.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+}
+function Manutencao2() {
+    var TxtEstado3 = document.querySelector('.manu2');
+    if (TxtEstado3.innerHTML === "Manutenção") {
+        TxtEstado3.innerHTML = "Desocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+    else if (TxtEstado3.innerHTML === "Desocupado") {
+        TxtEstado3.innerHTML = "Ocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else {
+        TxtEstado3.innerHTML = "Manutenção";
+        TxtEstado3.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+}
+function Manutencao3() {
+    var TxtEstado3 = document.querySelector('.manu3');
+    if (TxtEstado3.innerHTML === "Manutenção") {
+        TxtEstado3.innerHTML = "Desocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+    else if (TxtEstado3.innerHTML === "Desocupado") {
+        TxtEstado3.innerHTML = "Ocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else {
+        TxtEstado3.innerHTML = "Manutenção";
+        TxtEstado3.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+}
+function Manutencao4() {
+    var TxtEstado3 = document.querySelector('.manu4');
+    if (TxtEstado3.innerHTML === "Manutenção") {
+        TxtEstado3.innerHTML = "Desocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+    else if (TxtEstado3.innerHTML === "Desocupado") {
+        TxtEstado3.innerHTML = "Ocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else {
+        TxtEstado3.innerHTML = "Manutenção";
+        TxtEstado3.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
+}
+function Manutencao5() {
+    var TxtEstado3 = document.querySelector('.manu5');
+    if (TxtEstado3.innerHTML === "Manutenção") {
+        TxtEstado3.innerHTML = "Desocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(60, 161, 2)';
+    }
+    else if (TxtEstado3.innerHTML === "Desocupado") {
+        TxtEstado3.innerHTML = "Ocupado";
+        TxtEstado3.style.backgroundColor = 'rgb(145, 143, 143)';
+    }
+    else {
+        TxtEstado3.innerHTML = "Manutenção";
+        TxtEstado3.style.backgroundColor = 'rgb(196, 0, 0)';
+    }
 }
